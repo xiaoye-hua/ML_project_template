52291ca74c330d0e6d0cab90f866413357941ba0
xiaoye-hua <huag@kth.se>
2022-05-17 07:02:34 +0200

    change environment.yml

--- scripts/model_train.py
+++ scripts/model_train.py
@@ -26,10 +26,11 @@ train_valid = train_config_detail[dir_mark].get('train_valid', False)
 dense_features = train_config_detail[dir_mark].get('dense_features', None)
 sparse_features = train_config_detail[dir_mark].get('sparse_features', None)
 feature_clean_func = train_config_detail[dir_mark].get('feature_clean_func', None)
+additional_train_params = train_config_detail[dir_mark].get('additional_train_params', {})
+
 epochs = train_config_detail[dir_mark].get('epochs', None)
 batch_size = train_config_detail[dir_mark].get('batch_size', None)
 dense_to_sparse = train_config_detail[dir_mark].get('dense_to_sparse', None)
-additional_train_params = train_config_detail[dir_mark].get('additional_train_params', {})
 task = train_config_detail[dir_mark].get('task', None) # params for deepFM
 
 target_col = train_config_detail[dir_mark]['target_col']
